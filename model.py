2735fee5694a103baf51e003932615a51d1eacbd
tykim9106-bit <tykim9106@gmail.com>
2026-09-06 22:38:34 +0900

    Step 4: masked_sequence_logprob

--- model.py
+++ model.py
@@ -27,8 +27,9 @@ def gather_token_logprobs(log_probs, token_ids):
         axis=-1
     ).squeeze(-1)
 
-# Step 4 - masked_sequence_logprob (not yet solved)
-# TODO: implement
+# Step 4 - masked_sequence_logprob
+def masked_sequence_logprob(token_logprobs, mask):
+    return np.sum(token_logprobs * mask, axis=-1)
 
 # Step 5 - init_policy_params (not yet solved)
 # TODO: implement
